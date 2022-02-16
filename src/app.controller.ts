063fca05ef5b724ade9ae04b4a8e8115b5e6ecc7
Juan Camilo Posso <jucapo05@gmail.com>
2022-02-16 09:12:37 -0500

    test 5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './modules/auth/auth.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
+
+  
+  @Get('/')
+  async getHome() {
+    return this.appService.getHome();
+  }
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
